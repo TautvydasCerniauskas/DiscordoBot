5dcb79124c2cf719f2e5a07de0248ee68e6c97ff
greenavenue <tautvydas.cer@gmail.com>
2019-05-12 13:15:47 +0100

    Added gitignore and env support for the user token

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
-// Token Discord bot token
-var Token = "NTc2NDg2NDUyNjUzOTgxNjk3.XNXNvw.uL5hcU42pnz74cyQ9cDSboepSNk"
-
 func main() {
-	d, err := discordgo.New("Bot " + Token)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load env file", err)
+	}
+	token := os.Getenv("TOKEN")
+	d, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("Failed to create discord session", err)
 	}
